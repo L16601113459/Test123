c4d37717a18ade7b93e9264e29ccfdabd0e3b662
liudong <chrisliu0130@aliyun.com>
2018-03-22 19:23:45 +0800

    test

---
+++ src/com/atguigu/Test123123.java
@@ -0,0 +1,30 @@
+/**  
+* <p>Title: Test123123.java</p>  
+* <p>Description: 一句话描述</p>  
+* @author ASUS 
+* @date 2018年3月22日  
+* @version 1.0  
+*/
+package com.atguigu;
+
+/**
+ * @author ASUS
+ *
+ */
+public class Test123123 {
+
+	/**
+	 * <p>
+	 * Title: main
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test123123");
+	}
+
+}
